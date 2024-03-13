bf0242e87d16e04cdcab97b5e88d3dce870a0402
Viddhi Ladva <vidhiladva@gmail.com>
2024-03-13 15:45:10 +0530

    commit

--- src/E-commerce/Navbar.jsx
+++ src/E-commerce/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <nav className="bg-white border-gray-200 dark:bg-gray-800">
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <div
             className="flex items-center space-x-3 rtl:space-x-reverse"
--- src/E-commerce/ProductCard.jsx
+++ src/E-commerce/ProductCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ProductCard = () => {
   return (
     <div>
-      <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-gray-700">
     <img class="px-8 pt-10 rounded-t-lg" src="https://imgs.search.brave.com/05qyvTyZlfUL-BoLuWDoz6npkpZIhtFkv4JDv2kYSb4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTcx/NTg1NzU2L3Bob3Rv/L3dhdGNoLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz0yQU9a/SEx0ZTU5d1RMS1NK/QnFRbnQzd0c3T3Zq/SzJGQktoTTRWRjdP/Z1hRPQ" alt="product image" />
     <div class="px-5 py-5">
         <div class="text-xl tracking-tight text-white">
