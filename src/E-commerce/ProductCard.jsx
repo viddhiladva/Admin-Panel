f89f96fcf2df1dbb302a89f80429de4a053f8cbf
Viddhi Ladva <vidhiladva@gmail.com>
2024-03-14 14:38:13 +0530

    p-9

--- src/E-commerce/ProductCard.jsx
+++ src/E-commerce/ProductCard.jsx
@@ -3,26 +3,27 @@ import React from "react";
 const ProductCard = () => {
   return (
     <div>
-      <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-gray-700">
-    <img class="px-8 pt-10 rounded-t-lg" src="https://imgs.search.brave.com/05qyvTyZlfUL-BoLuWDoz6npkpZIhtFkv4JDv2kYSb4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTcx/NTg1NzU2L3Bob3Rv/L3dhdGNoLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz0yQU9a/SEx0ZTU5d1RMS1NK/QnFRbnQzd0c3T3Zq/SzJGQktoTTRWRjdP/Z1hRPQ" alt="product image" />
-    <div class="px-5 py-5">
-        <div class="text-xl tracking-tight text-white">
+      <div className="w-full max-w-sm border rounded-lg shadow bg-gray-100 ">
+        <img
+          className="px-8 pt-10 rounded-t-lg"
+          src="https://imgs.search.brave.com/05qyvTyZlfUL-BoLuWDoz6npkpZIhtFkv4JDv2kYSb4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTcx/NTg1NzU2L3Bob3Rv/L3dhdGNoLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz0yQU9a/SEx0ZTU5d1RMS1NK/QnFRbnQzd0c3T3Zq/SzJGQktoTTRWRjdP/Z1hRPQ"
+          alt="product image"
+        />
+        <div className="px-5 py-5">
+          <div className="text-xl tracking-tight text-black">
             Apple Watch Series 7 GPS
-        </div>
-        <div class="text-3xl font-bold text-white mt-1">
-            $599
-        </div>
-        <div class="flex justify-between gap-2 mt-4">
-            <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Add to cart
+          </div>
+          <div className="text-3xl font-bold text-black mt-1">$599</div>
+          <div className="flex justify-between gap-2 mt-4">
+            <button className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline">
+              Add to cart
             </button>
-            <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                View
+            <button className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline">
+              View
             </button>
+          </div>
         </div>
-    </div>
-</div>
-
+      </div>
     </div>
   );
 };
