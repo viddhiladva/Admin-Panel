33fcaf6c7cdd71a64e3d2385634fcbd14f6c662f
Viddhi Ladva <vidhiladva@gmail.com>
2024-03-14 14:59:37 +0530

    p-9

--- src/E-commerce/Home.jsx
+++ src/E-commerce/Home.jsx
@@ -5,7 +5,7 @@ import ProductCard from "./ProductCard";
 const Home = () => {
   return (
     <>
-      <div className=" h-screen bg-gray-400">
+      <div className=" h-screen bg-gray-200">
         <Navbar />
       <div className="flex justify-evenly items-center mt-5">
         <ProductCard />
