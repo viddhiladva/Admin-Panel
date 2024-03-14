528bcbe7b7407d836454a74de65e7881e86d3d4d
Viddhi Ladva <vidhiladva@gmail.com>
2024-03-14 15:06:35 +0530

    p-9

--- src/E-commerce/Home.jsx
+++ src/E-commerce/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <div className=" h-screen bg-gray-200">
         <Navbar />
       <div className="flex justify-evenly items-center mt-5">
-        <div className="grid grid-cols-5 gap-2">
+        <div className="grid grid-cols-5 gap-3">
         <ProductCard />
         <ProductCard />
         <ProductCard />
