cf48e2cc78825d8427384dc2f870a464ace32996
Viddhi Ladva <vidhiladva@gmail.com>
2024-03-14 18:09:31 +0530

    p-9

--- src/Components/ProductPanel/ShowProduct.jsx
+++ src/Components/ProductPanel/ShowProduct.jsx
@@ -2,8 +2,9 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProduct } from "./Slice/ProductSlice";
 import Sidebar from "../Admin/Sidebar";
-import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
-import Navbar from "../Admin/Navbar";
+import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
+import { AiOutlinePlus } from "react-icons/ai";
+import { BsFillCartPlusFill } from "react-icons/bs";
 
 const ShowProduct = () => {
   const dispatch = useDispatch();
@@ -17,32 +18,51 @@ const ShowProduct = () => {
     <>
       <div className="flex flex-col lg:flex-row bg-gray-200 min-h-screen">
         <Sidebar />
-        <div className="flex justify-center items-center h-full w-full my-auto">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-5">
-            {products.map((product) => (
-              <div
-                key={product.id}
-                className="bg-white rounded-lg shadow-md p-5"
-              >
-                <img
-                  src={product.image}
-                  alt={product.title}
-                  className="w-full h-44 object-cover mb-2 rounded"
-                />
-                <h2 className="text-2xl font-bold mb-2">{product.title}</h2>
-                <p className="text-gray-700 mb-2 text-xl font-semibold">{product.points}</p>
-                <p className="text-gray-700 mb-2">{product.description}</p>
-                <p className="text-gray-700 mb-2">Category : {product.category}</p>
-                <div className="flex justify-end">
-                  <button className="bg-green-600 text-white py-2 px-2 rounded mr-2">
-                    <AiOutlineEdit className="text-2xl" />
-                  </button>
-                  <button className="bg-red-500 text-white py-2 px-2 rounded">
-                    <AiOutlineDelete className="text-2xl" />
-                  </button>
+        <div className="w-full px-4 py-2 relative">
+          <div className="flex justify-end">
+            {/* Search Bar */}
+            {/* <input
+        type="text"
+        placeholder="Search..."
+        className="bg-white focus:outline-none border border-gray-300 rounded py-2 px-4 w-3/4 mr-2"
+      /> */}
+            {/* Add Product Button */}
+            <button className="bg-gray-800 text-white py-2 px-4 rounded flex justify-end">
+              <BsFillCartPlusFill className="text-2xl" />
+            </button>
+          </div>
+          {/* card */}
+          <div className="flex justify-center items-center h-full w-full my-auto relative">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 ">
+              {products.map((product) => (
+                <div
+                  key={product.id}
+                  className="bg-white rounded-lg shadow-md p-5"
+                >
+                  <img
+                    src={product.image}
+                    alt={product.title}
+                    className="w-full h-44 object-cover mb-2 rounded"
+                  />
+                  <h2 className="text-2xl font-bold mb-2">{product.title}</h2>
+                  <p className="text-gray-700 mb-2 text-xl font-semibold">
+                    {product.points}
+                  </p>
+                  <p className="text-gray-700 mb-2">{product.description}</p>
+                  <p className="text-gray-700 mb-2">
+                    Category : {product.category}
+                  </p>
+                  <div className="flex justify-end">
+                    <button className="bg-green-600 text-white py-2 px-2 rounded mr-2">
+                      <AiOutlineEdit className="text-2xl" />
+                    </button>
+                    <button className="bg-red-500 text-white py-2 px-2 rounded">
+                      <AiOutlineDelete className="text-2xl" />
+                    </button>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
